39aafcbd7ce105c80d144d704fc567fc6a2fc11b
sreemonta20 <sbhowmikcse08@gmail.com>
2020-12-30 22:40:05 +0100

    Add project files.

---
+++ ObjectArray/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectArray
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+
+        public override string ToString()
+        {
+            return "Customer ID is "+this.ID+" and Name is "+ this.Name;
+        }
+    }
+}
---
+++ ObjectArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object[] oArrObjects = new Object[3];
+            oArrObjects[0] = "1st Element";
+            oArrObjects[1] = 101;
+
+            Customer oCustomer =  new Customer();
+            oCustomer.ID = 1002;
+            oCustomer.Name = "Sreemonta";
+
+            oArrObjects[2] = oCustomer;
+
+            foreach (object item in oArrObjects)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+
+        }
+    }
+}
